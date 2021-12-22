e3f9046a30ad58e19bcd73d6a0cfc349d713b3e2
Matheus Araújo <apenasmatheus1@gmail.com>
2021-12-22 17:44:56 -0300

    :sparkles: Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
+import Header from './components/header/header';
 import './App.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/shop" element={<ShopPage />}></Route>
---
+++ src/components/header/header.tsx
@@ -0,0 +1,21 @@
+import './header.scss';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/084 crown.svg';
+
+const Header = () => (
+  <header className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <nav className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/contact">
+        CONTACT
+      </Link>
+    </nav>
+  </header>
+);
+
+export default Header;
