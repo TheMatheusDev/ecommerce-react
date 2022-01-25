87427d52374baa769496a7768d1fdc8d2264bc56
Matheus Araújo <apenasmatheus1@gmail.com>
2022-01-25 17:35:23 -0300

    :ambulance: Fix package.json error and remove logger from production build.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,6 +6,10 @@ import rootReducer from './root.reducer';
 
 const middlewares = [logger];
 
+if (process.env.NODE_env === 'development') {
+  middlewares.push(logger);
+}
+
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
 const persistor = persistStore(store);
